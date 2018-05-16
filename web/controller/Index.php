9aeb4810db9f815d8a49b982d32e49f49cd1acf7
蔡超 <2794807217@qq.com>
2018-05-16 17:44:41 +0800

    创建测试控制器

---
+++ web/controller/Index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caichao
+ * Date: 2018/5/16
+ * Time: 17:42
+ */
+namespace web\controller;
+class Index{
+    public function show(){
+        echo "12234";
+    }
+}
